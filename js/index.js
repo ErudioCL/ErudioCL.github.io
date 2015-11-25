345cecb5e658acd5f00bd6c7987de75742939a05
Cirno <tachibana.shuu@gmail.com>
2015-11-24 17:33:14 -0800

    primer commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){               
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+      $('.slider').slider({full_width: true});
+});
